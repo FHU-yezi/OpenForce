e96dd63fa733e33c698ceae5887c162247000adc
FHU-yezi <yehaowei20060411@qq.com>
2025-10-18 21:22:55 +0800

    feat(models): 添加对局记录结构体

---
+++ crates/models/src/battle_record.rs
@@ -0,0 +1,20 @@
+use constants::escape_result::EscapeResult;
+use constants::level::Level;
+use constants::map::Map;
+use constants::operator::Operator;
+use time::OffsetDateTime;
+
+pub struct BattleRecord {
+    id: String,
+    time: OffsetDateTime,
+    map: Map,
+    level: Level,
+    operator: Operator,
+    escape_result: EscapeResult,
+    duration_seconds: i16,
+    kill_operators_count: i16,
+    kill_bots_count: i16,
+    escape_value: i32,
+    net_profit: i32,
+    // teammates: Vec<Teammate>,
+}
