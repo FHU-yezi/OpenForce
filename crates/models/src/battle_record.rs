a8399f29d5136a5323dee34f280cb25440fff5e6
FHU-yezi <yehaowei20060411@qq.com>
2025-10-21 11:12:42 +0800

    feat(df-sdk): 为 `BattleRecord` 和 `Teammate` 实现 `Display` Trait

--- crates/models/src/battle_record.rs
+++ crates/models/src/battle_record.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 use constants::escape_result::EscapeResult;
 use constants::level::Level;
 use constants::map::Map;
@@ -44,4 +46,72 @@ where
         ))
         .map_err(serde::ser::Error::custom)?;
     serializer.serialize_str(&s)
+}
+
+impl Display for BattleRecord {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(
+            f,
+            "对局 {} - ID {}",
+            self.time
+                .format(format_description!(
+                    "[year]-[month]-[day]T[hour]:[minute]:[second]"
+                ))
+                .unwrap(),
+            self.id
+        )?;
+        // 首行长度固定，分隔线与首行长度相同
+        writeln!(f, "================================================")?;
+        writeln!(
+            f,
+            "地图：{} - {}\t干员：{}",
+            self.map.as_str(),
+            self.level.as_str(),
+            self.operator.as_str(),
+        )?;
+        writeln!(
+            f,
+            "{}{}对局时长：{} 秒",
+            self.escape_result.as_str(),
+            // 根据撤离结果调整制表符数量，保证输出对齐
+            match self.escape_result {
+                EscapeResult::EscapeSuccess | EscapeResult::MidwayExit => "\t\t",
+                _ => "\t",
+            },
+            self.duration_seconds,
+        )?;
+        writeln!(
+            f,
+            "击杀：干员 {} / AI {}",
+            self.kill_operators_count, self.kill_bots_count,
+        )?;
+        writeln!(f, "带出价值：{}", self.escape_value)?;
+        writeln!(f, "净收益：{}", self.escape_value)?;
+
+        if self.teammates.is_empty() {
+            writeln!(f, "无队友")?;
+        } else {
+            writeln!(f, "队友信息：")?;
+            for teammate in &self.teammates {
+                write!(f, "    {}", teammate)?;
+            }
+        }
+
+        Ok(())
+    }
+}
+
+impl Display for Teammate {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(
+            f,
+            "队友：{}  对局时长：{} 秒  {}  击杀：干员 {} / AI {}  带出价值：{}",
+            self.operator.as_str(),
+            self.duration_seconds,
+            self.escape_result.as_str(),
+            self.kill_operators_count,
+            self.kill_bots_count,
+            self.escape_value
+        )
+    }
 }
